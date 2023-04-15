99369cd5d4fabee224e25af0515165de8d468732
Ben Lovy <ben@deciduously.com>
2023-04-15 15:18:24 -0400

    updates

--- tg-mode.el
+++ tg-mode.el
@@ -1,12 +1,28 @@
+;;; tg-mode --- Summary
+;;; tg-mode is a major mode for editing Tangram typescript.
+;;; Commentary:
+;;; Must install the tg binary somewhere on your $PATH.
 ;; -*- lexical-binding: t; -*-
 ;;; tg-mode/config.el
+;;; Code:
 
+(require 'lsp-mode)
 (require 'typescript-mode)
 
-(define-derived-mode tg-mode typescript-mode "tg"
-  "Major mode for editing .tg files"
-  (setq-local lsp-command '("tg" "lsp")))
+(define-derived-mode tg-mode typescript-mode "Tangram Typescript"
+  "Major mode for editing .tg files.")
+ 
 
 (add-to-list 'auto-mode-alist '("\\.tg\\'" . tg-mode))
 
+(with-eval-after-load 'lsp-mode
+  (add-to-list 'lsp-language-id-configuration
+               '(tg-mode . "Tangram Typescript"))
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection '("tg" "lsp"))
+                    :activation-fn (lsp-activate-on "Tangram Typescript")
+                    :served-id 'tglsp)))
+
 (provide 'tg-mode)
+
+;;; tg-mode.el ends here
